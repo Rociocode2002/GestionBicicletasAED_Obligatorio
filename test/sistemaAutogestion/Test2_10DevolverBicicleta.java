db7c02381aed38e1af6d970ce90bbe1162a84ed1
Analía López <AL331850@fi365.ort.edu.uy>
2025-11-18 03:00:03 -0300

    tests 3.6 a 3.10

--- test/sistemaAutogestion/Test2_10DevolverBicicleta.java
+++ test/sistemaAutogestion/Test2_10DevolverBicicleta.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package sistemaAutogestion;
 
-/**
- *
- * @author rocio
- */
+package sistemaAutogestion;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class Test2_10DevolverBicicleta {
+     private Retorno retorno;
+    private final IObligatorio s = new Sistema();
+
+    @Before
+    public void setUp() {
+        s.crearSistemaDeGestion();
+    }
+    
+     @Test
+    public void devolverBicicletaOk(){
+        // Preparar escenario
+        s.registrarEstacion("Estacion01", "Ciudad Vieja", 5);
+        s.registrarUsuario("12345678", "Ana");
+        s.registrarBicicleta("BICI01", "URBANA");
+        s.asignarBicicletaAEstacion("BICI01", "Estacion01");
+        s.alquilarBicicleta("12345678", "Estacion01");
+
+        // Ejecutar método a testear
+        retorno = s.devolverBicicleta("12345678", "Centro");
+        assertEquals(Retorno.Resultado.OK, retorno.getResultado());
+        
+    }
     
 }
